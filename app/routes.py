79bb7cf04fca75b9598def89a2f17c762109ed8b
Eldor Raimov <2002el@list.ru>
2023-09-30 00:05:43 +0300

    Add private routes, updated database

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
 from flask import render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import app, db
-from app.models import User
+from app.models import User, Activity, UserActivity
 from app.forms import RegistrationForm, LoginForm
+from sqlalchemy.exc import OperationalError
+
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # Укажите маршрут для входа
+login_manager.login_message = 'Пожалуйста, войдите в систему, чтобы получить доступ к этой странице.'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route("/")
 @app.route("/home")
@@ -18,10 +28,11 @@ def register():
         username = form.username.data
         email = form.email.data
         password = form.password.data
-
+        role = "Employee"
+        
         hashed_password = generate_password_hash(password, method='sha256')
 
-        new_user = User(username=username, email=email, password=hashed_password)
+        new_user = User(username=username, email=email, password=hashed_password, role=role)
         db.session.add(new_user)
         db.session.commit()
 
@@ -32,24 +43,42 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
 
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
+    try:
+        if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
 
-        user = User.query.filter_by(username=username).first()
+            user = User.query.filter_by(username=username).first()
 
-        if user and check_password_hash(user.password, password):
-            flash("Авторизация успешна!", "success")
-            session['user_id'] = user.id
-            return redirect(url_for("home"))
-        else:
-            flash("Неверные учетные данные. Пожалуйста, проверьте свое имя пользователя и пароль.", "danger")
+            if user and check_password_hash(user.password, password):
+                flash("Авторизация успешна!", "success")
+                login_user(user)
+                session['user_id'] = user.id
+                return redirect(url_for("home"))
+            else:
+                flash("Неверные учетные данные. Пожалуйста, проверьте свое имя пользователя и пароль.", "danger")
+
+    except OperationalError as e:
+        # Обработка ошибок базы данных
+        flash("Произошла ошибка при работе с базой данных.", "danger")
+        # Вывести информацию об ошибке для отладки:
+        print(f"Database error: {e}")
 
     return render_template("login.html", form=form)
 
 @app.route("/logout")
+@login_required
 def logout():
-    session.clear()
+    logout_user()
     return redirect(url_for("home"))
+
+@app.route('/private')
+@login_required
+def method_name():
+    return "Приватная страница"
